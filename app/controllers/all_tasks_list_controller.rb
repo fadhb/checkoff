7fdcab5304da522e9f1c688d95f25a8ad58078ce
Jonathan Headen <jonathan.headen@outlook.com>
2014-05-02 21:55:40 +1000

    main index for choosing tasks

--- app/controllers/all_tasks_list_controller.rb
+++ app/controllers/all_tasks_list_controller.rb
@@ -1,4 +1,5 @@
 class AllTasksListController < ApplicationController
   def index
+    @tasks = Task.order(:title)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Checkoff::Application.routes.draw do
   get "all_tasks_list/index"
   resources :tasks
 
+  root 'all_tasks_list#index', as: 'TaskChooser'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
